6f4e32c51a8f5daf04f65cab46e72e8be36a1dfc
Xico <francisco100eg@gmail.com>
2023-04-17 21:38:04 -0300

    Feat: Added model for vheicle

---
+++ api/models/Vheicle.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const vheicleSchema = new Schema(
+  {
+    brand: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    model: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    year: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    plate: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    userId: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+    },
+    },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Vheicle", vheicleSchema);
