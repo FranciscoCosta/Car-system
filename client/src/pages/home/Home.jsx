d762b798341e276b04beae5714bf514aac39d3ca
Xico <francisco100eg@gmail.com>
2023-04-18 21:21:10 -0300

    Fix: Homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,57 +1,74 @@
 import React from "react";
 import "./Home.scss";
-import {hero, Logo} from "../../assets/index";
-import { BsFillFileTextFill } from 'react-icons/bs';
-import {MdCarRepair} from 'react-icons/md';
-import { BiTimer } from 'react-icons/bi';
+import { hero, Logo } from "../../assets/index";
+import { BsFillFileTextFill } from "react-icons/bs";
+import { MdCarRepair } from "react-icons/md";
+import { BiTimer } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <div className="Home">
       <div className="Home__container">
         <div className="Home__container-hero">
-        <div className="container-info">
-          <img src={Logo} alt="logo" />
-          <p>
-            Experimente o <span>AutoMatch</span> e facilite a manutenção do seu automóvel!
-            <br/>
-            Simplifique sua vida e mantenha seu carro em dia,
-            utilize agora o <span>AutoMatch</span>!
-          </p>
-          <button
-            onClick={() => { navigate("/register")}}
-          >REGISTRAR</button>
-          </div>
-          <div className="Hero__img">
-          <img src={hero} alt="hero-png" />
-          </div>
+          <motion.div
+            whileInView={{ y: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 1 }}
+            className="container-info"
+          >
+            <img src={Logo} alt="logo" />
+            <p>
+              Experimente o <span>AutoMatch</span> e facilite a manutenção do
+              seu automóvel!
+              <br />
+              Simplifique sua vida e mantenha seu carro em dia, utilize agora o{" "}
+              <span>AutoMatch</span>!
+            </p>
+            <button
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              REGISTRAR
+            </button>
+          </motion.div>
+          <motion.div
+            whileInView={{ x: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.8 }}
+            className="Hero__img"
+          >
+            <img src={hero} alt="hero-png" />
+          </motion.div>
         </div>
         <div className="Home__container-service">
           <h2>Serviços</h2>
-          <div className="container-service">
+          <motion.div
+            whileInView={{ y: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className="container-service"
+          >
             <div className="service-card">
               <h3>Orçamento</h3>
               <BsFillFileTextFill size={50} />
               <p>
-                Solicite um orçamento para a manutenção do seu veículo e aguarde.
+                Solicite um orçamento para a manutenção do seu veículo e
+                aguarde.
               </p>
-            </div>  
+            </div>
             <div className="service-card">
               <h3>Reparação</h3>
               <BiTimer size={50} />
               <p>
                 Melhores reparações com os melhores profissionais, agende agora!
               </p>
-          </div>
-          <div className="service-card">
-            <h3>Manutenção</h3>
-            <MdCarRepair size={50} />
-            <p>
-              Mantenha seu veículo em dia, agende a sua manutenção .
-            </p>
             </div>
+            <div className="service-card">
+              <h3>Manutenção</h3>
+              <MdCarRepair size={50} />
+              <p>Mantenha seu veículo em dia, agende a sua manutenção .</p>
             </div>
-            </div>
+          </motion.div>
+        </div>
         <div className="Home__container-contato">
           <h2>Contato</h2>
         </div>
